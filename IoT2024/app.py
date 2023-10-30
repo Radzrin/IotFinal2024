510f45d6c1b2bb576836018b24defbd39791d6e9
Radzrin <60394053+Radzrin@users.noreply.github.com>
2023-10-30 19:01:20 -0400

    Add files via upload

--- IoT2024/app.py
+++ IoT2024/app.py
@@ -1,12 +1,8 @@
-from dash import Dash, html, dcc, Input, Output, callback, DiskcacheManager, CeleryManager
-import plotly.graph_objects as go
-import dash_daq as daq
-import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, Input, Output, DiskcacheManager, CeleryManager, callback
 import RPi.GPIO as GPIO
 from gpiozero import LED
 import Freenove_DHT as DHT
-import DHT11 as DHT11
-# import bluetooth
+import bluetooth
 import smtplib
 import time as time
 import imaplib
@@ -15,20 +11,21 @@ from email.header import decode_header
 import webbrowser
 import os
 
-emailadd = "iotproject2024@outlook.com"
-password = "Project4iot2024"
+emailadd = ""
+password = ""
 
-# GPIO WARNING OFF (ignore this part)
+
+#GPIO WARNING OFF (ignore this part)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+
+# GPIO setup
 LED = 12
 GPIO.setup(LED, GPIO.OUT)
 
-DHTPin = 13 
-dht = DHT.DHT(DHTPin) 
+DHTin =  13 # DHT pin
 
-# Motor Setup
 Motor1 = 22 # Enable Pin
 Motor2 = 27 # Input Pin
 Motor3 = 17 # Input Pin
@@ -38,9 +35,21 @@ GPIO.setup(Motor3,GPIO.OUT)
 
 isSent = False #check to ony send 1 email
 
+
 def clean(text):
 	# clean text for creating a folder
 	return "".join(c if c.isalnum() else "_" for c in text)
+
+
+
+#on and off LED Image
+ledonImg = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F03%2FLight-Bulb-PNG-File.png&f=1&nofb=1&ipt=4b6dfaef65c3415b004cf1ace7ec7e574e72761bd0b4a499e421ad558bdb6188&ipo=images"
+ledoffImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjooinn.com%2Fimages%2Fisolated-light-bulb-1.jpg&f=1&nofb=1&ipt=f78dfa9fd703b7f469b6fdba546220e119555a0d638393e27a9b653beac80ffe&ipo=images"
+
+fanImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ficon-library.com%2Fimages%2Ffan-icon-png%2Ffan-icon-png-2.jpg"
+
+
+
 
 def openMot():
 	GPIO.output(Motor1,GPIO.HIGH)
@@ -68,158 +77,116 @@ def send():
 
 		smtp.sendmail(emailadd, emailadd, msg)
 
-# App
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+app = Dash(__name__)
+
+app.head = [html.Link(rel='stylesheet', href='iotcss.css')]
 
 app.layout = html.Div(children=[
-    dbc.Navbar(
-        dbc.Container(
-            [
-                html.A(
-                    dbc.Row(
-                        [
-                            dbc.Col(dbc.NavbarBrand("IoT - Dashboard", className="ms-2")),
-                        ],
-                        align="center",
-                        className="g-0",
-                    ),
-                    style={"textDecoration": "none"},
-                )
-            ],
-        ),
-        color="#393646",
-        dark=True,
-    ),
-    
-    
-    # Top Div
-    html.Div(children=[
-        html.H1("User", "header"),
-        html.Div(children=[
-            html.Img(src="assets/userIcon.png", id="userIcon"),
-        ])
-    ],id='topDiv'),
-    html.Div(children=[
-        html.Div(children=[
-            html.H1("Temperature and Humidity", "header"),
-            html.Div(children=[
-                html.Div([
-                    daq.Gauge(
-                        showCurrentValue=True,
-                        color={"gradient":True,"ranges":{"blue":[-10,-24],"green":[-24,24],"red":[24,40]}},
-                        id='temp-gauge',
-                        label="Temperature",
-                        value=0,
-                        min=-10,
-                        max=40,
-                        units="°C",
-                        size=150,
-                        theme="dark"
-                    ),
-                ], id="temp-gaugeDiv"),
-				html.Div([
-                    daq.Gauge(
-                        showCurrentValue=True,
-                        color={"gradient":True,"ranges":{"blue":[-20,-24],"green":[-24,24],"red":[24,40]}},
-                        id='hum-gauge',
-                        label="Humidity",
-                        value=0,
-                        min=-20,
-                        max=100,
-                        units="°C",
-                        size=150
-                    ),
-                ], id="hum-gaugeDiv"),
-				html.P("Does it Work?"),
-                html.Div(id='testP1'),
-            ], className="gaugeContainer"),
-            
-        ],id='temp-humDiv'),
-
-        # Fan Div
-        html.Div(children=[
-            html.H1("Fan", "header"),
-            html.Div(children=[
-                html.Img(style={'width': '200px', 'height': '200px'}, src="assets/fan.png", className="rotate-image", id="rotate-image"),
-            ], id="fanDiv"),
-            daq.ToggleSwitch(id='toggle-switch')
-        ], id="div1"),
-	html.Div(id='testP'),
-        
-        # Light Div
-        html.Div(children=[
-            html.H1("Light", "header"),
-            html.Div([
-                html.Img(id='image-display'),
-            ], id="lightDiv"),
-            daq.ToggleSwitch(id='image-toggle')
-        ], id="div2"),
-    ], className="widgetContainer"),
-	# Testing
-
-	# html.Div(id='testT'),
-	
-    dcc.Interval(
-        id='intervalDiv',
-        interval=1000,
-        n_intervals=0
-    )
-
+	html.Nav([
+		html.H3("Iot Project")
+		]),
+	html.Div(id="wrapper", children=[
+		html.Div(id='user',
+			 children=[
+				 html.Img(id='pfp', src="#"),
+				 html.Ul(id="userPref", children=[
+					html.Li("Username"),
+					html.Li("Light Treshold"),
+					html.Li("Temeraure Treshold"),
+					html.Li("Humidity Treshold"),
+					html.Li("s")
+				 ])
+				 ]),
+		html.Div(id='ledDiv', children=[
+			html.H2("LED"),
+			html.Button(children=[
+				html.Img(src=ledoffImg, id='btnimg', height='200px')
+				], id='submit-val', n_clicks=0)
+			]),
+		html.Div(id='weatherDiv', children=[
+			html.H2("Weather"),
+			html.Img(src= fanImg, id='fanimg'),
+			html.Div(id='testP'),
+			html.Div(id='testP1'),
+			html.Div(id='bluetoothDiv'),
+			])
+		]),#interval that updates the code every 1000ms (1s)
+		dcc.Interval(
+			id='interv',
+			interval=1000,
+			n_intervals=0
+		)
 ])
-    
-#Turn LED on and off whenever the user presses the button on the dashboard
-@app.callback(Output('image-display', 'src'),
-    Input('image-toggle', 'value'))
-    
-def update_image(toggle_value):
-    lightOn = 'assets/lightOn.png'
-    lightOff = 'assets/lightOff.png'
-    
-    if toggle_value:
-        GPIO.output(LED, GPIO.HIGH)
-        return lightOn
-    else:
-        GPIO.output(LED, GPIO.LOW) 
-        return lightOff
 
 
+
+#turn LED on and off whenever the user presses the button on the dashboard
+@callback(
+	Output('btnimg', 'src'),
+	Input('submit-val', 'n_clicks'),
+	prevent_initial_call=True
+)
+def update_output(n_clicks):
+	if n_clicks % 2:
+		GPIO.output(LED, GPIO.LOW)
+		return ledoffImg
+	else:
+		GPIO.output(LED, GPIO.HIGH)
+		return ledonImg
+	
+	return 0
+			
 # Change the temperature in real time and send email if it reaches a certain treshold (10*C)
 # Read the temperature in real time and send email if it reaches a certain treshold (24*C)
-@app.callback(Output('temp-gauge', 'value'),
-    Input('intervalDiv', 'n_intervals'),
-    prevent_initial_call=True
-)
-
+@callback(
+	Output('testT', 'children'),
+	Input('interv', 'n_intervals'),
+	prevent_initial_call=True
+ )           
 def FanCheck(inVal):
-    isSent = False
-    for i in range(0,15):
-        chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-        if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-            break
-        time.sleep(0.1)
-    print("Temperature : %.2f\n"%(dht.temperature))
+
+	dht = DHT.DHT(DHTin)
 	
-    if(dht.temperature >= 24 and isSent == False):
-        isSent = True # only send 1 email
-        send()
-        return 0
-    return dht.temperature
+	isSent = False
+	
+	counts = 0 # Measurement counts
+	while(True):
+		counts += 1
+		for i in range(0,15):
+			chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+			if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+				break
+			time.sleep(0.1)
+		print("Temperature : %.2f \n"%(dht.temperature))
+		if(dht.temperature >= 24 and isSent == False):
+			isSent = True # only send 1 email
+			send()
+			return 0
+		return dht.temperature
 
 
 # Read the humidity in real time
-@app.callback(Output('hum-gauge', 'value'),
-    Input('intervalDiv', 'n_intervals'),
-    prevent_initial_call=True
- )      
-     
+@callback(
+	Output('testH', 'children'),
+	Input('interv', 'n_intervals'),
+	prevent_initial_call=True
+ )           
 def humidityCheck(inVal):
-    for i in range(0,15):
-        chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-        if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-            break
-        time.sleep(0.1)
-    print("Humidity : %.2f\n"%(dht.humidity))
-    return dht.humidity
+
+	dht = DHT.DHT(DHTin)
+	
+	counts = 0 # Measurement counts
+	while(True):
+		counts += 1
+		for i in range(0,15):
+			chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+			if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+				break
+			time.sleep(0.1)
+		print("Humidity : %.2f\n"%(dht.humidity))
+		return dht.humidity
 
 '''
 @callback(
@@ -235,30 +202,14 @@ def scan(n):
 	return f'{number_of_devices} devices found'     
 '''
 
-# TODO: At the moment, the fan is only being turned on by a toggle switch. Change when email is received.
-# every second, check if the email was sent
-# if yes read the email
-# else set isSent to True and send an email
-@app.callback(
-    Output('rotate-image', 'className'),
-    Input('toggle-switch', 'value')
-)
-
-def update_image_rotation(toggle_value):
-    if toggle_value:
-        return "rotate-image"
-    else:
-        return ""
-
 # every second, check if the email was sent
 # if yes read the email
 # else set isSent to True and send an email
 @callback(
 	Output('testP1', 'children'),
-	Input('intervalDiv', 'n_intervals'),
+	Input('interv', 'n_intervals'),
 	prevent_initial_call=True
-)
-
+ )      
 def check(default):
 	global isSent
 
@@ -329,6 +280,10 @@ def check(default):
 	imap.close()
 	imap.logout()
 
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
+	app.run(debug=True)
+
+
+
+
+   
